09f4838b1e4623d41aff75298e82e311574792b8
DaiToto <1500012781@pku.edu.cn>
2018-06-05 15:21:31 +0800

    var_list

---
+++ main.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from net import build_generator, build_discriminator
+import cv2
+import numpy as np
+
+plain = np.zeros((210, 48, 64, 1), float)
+real = np.zeros((210, 48, 64, 1), float)
+batch_size = 10
+
+def predo(tot_num = 200):
+
+	for i in range(tot_num):
+		img = cv2.imread('plain/' + str(i).zfill(4) + '.png')
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		img = cv2.resize(img, (64, 48))
+		plain[i] = np.reshape(img, (48, 64, 1))
+		img = cv2.imread('real/' + str(i).zfill(4) + '.png')
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		img = cv2.resize(img, (64, 48))
+		real[i] = np.reshape(img, (48, 64, 1))
+	print("read done...")
+
+def readin(tot_num = 200):
+
+	index = np.arange(tot_num)
+	np.random.shuffle(index)
+	return plain[index], real[index]
+
+
+def train(restore = False, K = 1):
+	ginputs = tf.placeholder(tf.float32, [batch_size, 48, 64, 1])
+	dinputs = tf.placeholder(tf.float32, [batch_size, 48, 64, 1])
+	keep_prob = tf.placeholder(tf.float32)
+
+	g_out, g_params = build_generator(ginputs)
+	d_out, d_params = build_discriminator(dinputs, g_out, keep_prob)
+	d_real = tf.slice(d_out, [0, 0], [batch_size, -1])
+	d_fake = tf.slice(d_out, [batch_size, 0], [-1, -1])
+
+	d_loss = -(tf.log(d_real) + tf.log(1 - d_fake))
+	g_loss = -tf.log(d_fake)
+
+	optimizer = tf.train.AdamOptimizer()
+
+	d_train = optimizer.minimize(d_loss, var_list = d_params)
+	g_train = optimizer.minimize(g_loss, var_list = g_params)
+
+	saver = tf.train.Saver()
+	with tf.Session() as sess:
+		if restore:
+			saver.restore(sess, check_path)
+		else:
+			sess.run(tf.global_variables_initializer())
+			print('initial done')
+
+			for step in range(maxiter):
+				plain, real = readin()
+				for iters in range(200 // batch_size):
+					plain1 = plain[iters * batch_size: (iters + 1) * batch_size]
+					real1 = real[iters * batch_size: (iters + 1) * batch_size]
+					print(step, iters)
+					feed = {ginputs: plain1,
+							dinputs: real1,
+							keep_prob: 0.5}
+					loss, _ = sess.run([d_loss, d_train], feed_dict = feed)
+					print('D Loss: ', loss)
+					for j in range(K):
+						loss, _ = sess.run([g_loss, g_train], feed_dict = feed)
+						print('G Loss:', loss)
+
+				if(step % log_n == 0):
+					test_G = {ginputs: plain[:batch_size],
+							  dinputs: plain[:batch_size],
+							  keep_prob: 1.0}
+					out_G = sess.run([g_out], feed_dict = test_G)
+					save_img(out_G, step)
+				saver.save(sess, check_path, global_step = step)
+
+def main():
+	predo()
+	train()
+	# test()
+
+if __name__ == '__main__':
+	main()
---
+++ net.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import numpy as np
+
+def conv_layer(inputs, shape, stride, padding='SAME', name=None):
+	filter_size = tf.TensorShape(shape)
+	f = tf.Variable(tf.truncated_normal(filter_size, stddev = 0.1), name = "filter")
+	conv = tf.nn.conv2d(inputs, f, stride, padding, name=name)
+	return conv, f
+
+def build_generator(ginputs):
+	stride = [1, 1, 1, 1]
+	params = []
+	with tf.name_scope('G_conv1'):
+		G_conv1, f = conv_layer(ginputs, [3, 3, 1, 16], stride, padding='SAME')
+		G_conv1 = tf.nn.leaky_relu(G_conv1)
+		G_pool1 = tf.nn.max_pool(G_conv1, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+		params.append(f)
+
+	with tf.name_scope('G_conv2'):
+		G_conv2, f = conv_layer(G_pool1, [3, 3, 16, 32], stride, padding='SAME')
+		G_conv2 = tf.nn.leaky_relu(G_conv2)
+		G_pool2 = tf.nn.max_pool(G_conv2, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+		params.append(f)
+
+	with tf.name_scope('G_conv3'):
+		G_conv3, f = conv_layer(G_pool2, [3, 3, 32, 64], stride, padding='SAME')
+		G_conv3 = tf.nn.leaky_relu(G_conv3)
+		G_pool3 = tf.nn.max_pool(G_conv3, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME')
+		params.append(f)
+
+	with tf.name_scope('G_conv4'):
+		G_conv4_1, f = conv_layer(G_pool3, [3, 3, 64, 128], stride, padding='SAME')
+		G_conv4_1 = tf.nn.leaky_relu(G_conv4_1)
+		params.append(f)
+		G_conv4_2, f = conv_layer(G_conv4_1, [3, 3, 128, 128], stride, padding='SAME')
+		G_conv4_2 = tf.nn.leaky_relu(G_conv4_2)
+		params.append(f)
+
+	#upsample
+	with tf.name_scope('G_deconv3'):
+		f_deconv3_shape = [3, 3, 64, 128]
+		f_deconv3_shape = tf.TensorShape(f_deconv3_shape)
+		f_deconv3 = tf.Variable(tf.truncated_normal(f_deconv3_shape, stddev=0.1))
+		G_deconv3 = tf.nn.conv2d_transpose(G_conv4_2, f_deconv3, G_conv3.get_shape(), [1, 2, 2, 1], padding='SAME')
+		params.append(f_deconv3)
+		G_deconv3 = tf.concat([G_deconv3, G_conv3], axis=3)
+		G_deconv3, f = conv_layer(G_deconv3, [3, 3, 128, 64], stride, padding='SAME')
+		G_deconv3 = tf.nn.leaky_relu(G_deconv3)
+		params.append(f)
+
+	with tf.name_scope('G_deconv2'):
+		f_deconv2_shape = [3, 3, 32, 64]
+		f_deconv2_shape = tf.TensorShape(f_deconv2_shape)
+		f_deconv2 = tf.Variable(tf.truncated_normal(f_deconv2_shape, stddev=0.1))
+		G_deconv2 = tf.nn.conv2d_transpose(G_deconv3, f_deconv2, G_conv2.get_shape(), [1, 2, 2, 1], padding='SAME')
+		params.append(f_deconv2)
+		G_deconv2 = tf.concat([G_deconv2, G_conv2], axis=3)
+		G_deconv2, f = conv_layer(G_deconv2, [3, 3, 64, 32], stride, padding='SAME')
+		G_deconv2 = tf.nn.leaky_relu(G_deconv2)
+		params.append(f)
+
+	with tf.name_scope('G_deconv1'):
+		f_deconv1_shape = [3, 3, 16, 32]
+		f_deconv1_shape = tf.TensorShape(f_deconv1_shape)
+		f_deconv1 = tf.Variable(tf.truncated_normal(f_deconv1_shape, stddev=0.1))
+		G_deconv1 = tf.nn.conv2d_transpose(G_deconv2, f_deconv1, G_conv1.get_shape(), [1, 2, 2, 1], padding='SAME')
+		params.append(f_deconv1)
+		G_deconv1 = tf.concat([G_deconv1, G_conv1], axis=3)
+		G_deconv1, f = conv_layer(G_deconv1, [3, 3, 32, 16], stride, padding='SAME')
+		G_deconv1 = tf.nn.leaky_relu(G_deconv1)
+		params.append(f)
+
+	out, f = conv_layer(G_deconv1, [1, 1, 16, 1], stride, padding='SAME')
+	params.append(f)
+	out = tf.nn.sigmoid(out)
+	return out, params
+
+def build_discriminator(dinputs, gouts, keep_prob):
+	d_params = []
+	stride = [1, 1, 1, 1]
+	conv = tf.concat([dinputs, gouts], 0)
+	channel_in = 1
+	channel_out = 16
+	for i in list(range(1,4)):
+		with tf.name_scope('D_conv%d' % i):
+			conv, f = conv_layer(conv, [3, 3, channel_in, channel_out], stride, padding='SAME', name='D_conv%d' % i)
+			conv = tf.nn.leaky_relu(conv, name='D_leaky_relu%d' % i)
+			conv = tf.nn.max_pool(conv, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', name='D_pool%d' % i)
+			conv = tf.nn.dropout(conv, keep_prob)
+			d_params.append(f)
+			channel_in = channel_out
+			channel_out *= 2
+
+	dense = tf.reduce_mean(conv, axis=[1, 2])
+	with tf.name_scope('D_dense'):
+		wshape = [dense.get_shape()[-1], 2]
+		wshape = tf.TensorShape(wshape)
+		weight = tf.Variable(tf.truncated_normal(wshape, stddev=0.1))
+		dense = tf.matmul(dense, weight)
+		d_params.append(weight)
+		bias = tf.Variable(tf.truncated_normal([2], stddev=0.1))
+		dense = tf.nn.bias_add(dense, bias)
+		d_params.append(bias)
+		dense = tf.nn.sigmoid(dense)
+	return dense, d_params
+
