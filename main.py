bebdc12dd84c33c9952a25b65bf18972360197cc
DaiToto <1500012781@pku.edu.cn>
2018-06-06 19:42:06 +0800

    2

--- main.py
+++ main.py
@@ -93,8 +93,9 @@ def train(restore = False, K = 1):
 							  dinputs: real1,
 							  keep_prob: 1.0}
 					out_G = sess.run([g_out], feed_dict = test_G)
-					out_G = (out_G - 1) / 2 * 255
 					print(np.shape(out_G))
+					out_G = (out_G - 1) / 2 * 255
+
 					print(out_G[0])
 					save_img(out_G, step)
 				saver.save(sess, check_path, global_step = step)
